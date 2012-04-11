56a4e4ace0bdbedad946771fe94d54c97155b014
kn65op <kn65op@gmail.com>
2012-04-11 13:18:44 +0200

    Change concept

--- src/extras.cpp
+++ src/extras.cpp
@@ -2,32 +2,32 @@
 
 using namespace TALSA;
 
-snd_pcm_format_t getFormat(DataFormat df)
-{
-  switch (df)
-  {
-    case DataFormat::U8:
-      return SND_PCM_FORMAT_U8;
-    case DataFormat::U16:
-      return SND_PCM_FORMAT_U16;
-    case DataFormat::U24:
-      return SND_PCM_FORMAT_U24;
-    default:
-      throw InvalidOperation("Wrong data format");
-  }
-}
+//snd_pcm_format_t DataFormatHelper::getFormat(DataFormat df)
+//{
+//  switch (df)
+//  {
+//    case DataFormat::U8:
+//      return SND_PCM_FORMAT_U8;
+//    case DataFormat::U16:
+//      return SND_PCM_FORMAT_U16;
+//    case DataFormat::U24:
+//      return SND_PCM_FORMAT_U24;
+//    default:
+//      throw InvalidOperation("Wrong data format");
+//  }
+//}
 
-int getSizeOfFormat(DataFormat df)
-{
-  switch (df)
-  {
-    case DataFormat::U8:
-      return 1;
-    case DataFormat::U16:
-      return 2;
-    case DataFormat::U24:
-      return 4; //zgodnie z dokumentacją
-    default:
-      throw InvalidOperation("Wrong data format");
-  }
-}
+//int DataFormatHelper::getSizeOfFormat(DataFormat df)
+//{
+//  switch (df)
+//  {
+//    case DataFormat::U8:
+//      return 1;
+//    case DataFormat::U16:
+//      return 2;
+//    case DataFormat::U24:
+//      return 4; //zgodnie z dokumentacją
+//    default:
+//      throw InvalidOperation("Wrong data format");
+//  }
+//}
